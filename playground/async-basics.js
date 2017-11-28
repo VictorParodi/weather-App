d2132828f072a5ba9190387409a1f0d3875a8b70
Victor <victor.parodi.montero@gmail.com>
2017-11-28 05:41:38 -0500

    Commit inicial

---
+++ playground/async-basics.js
@@ -0,0 +1,11 @@
+console.log('Starting app');
+
+setTimeout(() => {
+    console.log('After 2 seconds');
+}, 2000);
+
+setTimeout(() => {
+    console.log('After 0 seconds');
+}, 0);
+
+console.log('Finising up');
