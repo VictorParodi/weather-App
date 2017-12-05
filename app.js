dfc98977ffb99d1aaf7e34d7dc214adde92b7b3a
Victor <victor.parodi.montero@gmail.com>
2017-12-05 06:31:02 -0500

    master commit 3
    Instalando el paquete yargs.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const request = require('request');
+const yargs = require('yargs');
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitud: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitud: ${body.results[0].geometry.location.lng}`);
 });
